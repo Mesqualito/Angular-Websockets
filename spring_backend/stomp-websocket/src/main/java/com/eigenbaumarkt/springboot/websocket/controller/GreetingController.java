74fe4e9514ece42c8ede85f4b841eea5ff57b419
Mesqualito <jochen@gebsattel.rocks>
2019-12-20 14:26:43 +0100

    SpringBoot Backend & Angular 8 Frontend connected with Websocket
    => https://www.youtube.com/watch?v=r2tPEfVgsIE
    => https://www.javaguides.net/2019/06/spring-boot-angular-8-websocket-example-tutorial.html
    
    => https://github.com/RameshMF/angular8-springboot-websocket

---
+++ spring_backend/stomp-websocket/src/main/java/com/eigenbaumarkt/springboot/websocket/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.eigenbaumarkt.springboot.websocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.eigenbaumarkt.springboot.websocket.model.Greeting;
+import com.eigenbaumarkt.springboot.websocket.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
